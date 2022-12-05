7e6e2243c4715af1a384bd14ac4580a89b19fdb7
Luis Simosa <simosa37@gmail.com>
2022-12-05 14:29:20 -0300

    fixed authform width

--- src/components/containers/AuthForm.styled.tsx
+++ src/components/containers/AuthForm.styled.tsx
@@ -4,7 +4,8 @@ export const StyledForm = styled.form`
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    width: 50%;
+    width: 40%;
+    max-width: 400px;
     margin: 2rem auto;
 
     @media (max-width: 700px) {
