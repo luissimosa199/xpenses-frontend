cddb2cfc52cd6bb0ea0a45af723fbbc814a54d7f
Luis Simosa <simosa37@gmail.com>
2022-12-26 16:45:15 -0300

    user data now persist in localstorage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,6 +16,7 @@ const Header: FunctionComponent = () => {
     localStorage.removeItem("name");
     localStorage.removeItem("token");
     localStorage.removeItem("families");
+    localStorage.removeItem("userData");
     handleLogout();
     navigate("/login", { replace: true });
   };
--- src/utils/RequireAuth.tsx
+++ src/utils/RequireAuth.tsx
@@ -1,10 +1,24 @@
-import { FunctionComponent } from "react"
-import { Navigate } from "react-router-dom"
+import { FunctionComponent, useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 interface RequireAuthProps {
-  children: JSX.Element
+  children: JSX.Element;
 }
 
-export const RequireAuth: FunctionComponent<RequireAuthProps> = ({ children }) => {
-    return !localStorage.getItem('token') ? <Navigate to='/login' replace={true} /> : children
-}
+export const RequireAuth: FunctionComponent<RequireAuthProps> = ({
+  children,
+}) => {
+
+  const { handleLogin } = useContext(UserContext);
+
+  if (!localStorage.getItem("token") || !localStorage.getItem('userData')) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
+  const loginData = localStorage.getItem('userData')
+  
+  handleLogin(JSON.parse(loginData as string));
+
+  return children;
+};
